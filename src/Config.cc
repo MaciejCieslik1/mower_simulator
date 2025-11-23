de192ea05a47763d7f6f4d5e0a95be6aef8ad14c
Maciej <01187214@pw.edu.pl>
2025-11-23 22:19:49 +0100

    Moved local constant variables to Constants file

--- src/Config.cc
+++ src/Config.cc
@@ -25,20 +25,14 @@ namespace Config {
     unsigned int MAX_SPEED = 0;
 
     void initializeRuntimeConstants(const unsigned int& lawn_width, const unsigned int& lawn_length) {
+        MIN_BLADE_DIAMETER = max(Constants::ABSOLUTE_MIN_BLADE_DIAMETER, 
+            min(lawn_width / Constants::MIN_LAWN_DIVISION_FACTOR, lawn_length / Constants::MIN_LAWN_DIVISION_FACTOR));
 
-        const unsigned int ABSOLUTE_MIN_BLADE_DIAMETER = 10; // cm
-        const unsigned int MIN_LAWN_DIVISION_FACTOR = 1000;
-        MIN_BLADE_DIAMETER = max(ABSOLUTE_MIN_BLADE_DIAMETER, 
-            min(lawn_width / MIN_LAWN_DIVISION_FACTOR, lawn_length / MIN_LAWN_DIVISION_FACTOR));
+        MAX_BLADE_DIAMETER = min(Constants::ABSOLUTE_MAX_BLADE_DIAMETER, 
+            min(lawn_width / Constants::MAX_LAWN_DIVISION_FACTOR, lawn_length / Constants::MAX_LAWN_DIVISION_FACTOR));
 
-        const unsigned int ABSOLUTE_MAX_BLADE_DIAMETER = 100; // cm
-        const unsigned int MAX_LAWN_DIVISION_FACTOR = 10;
-        MAX_BLADE_DIAMETER = min(ABSOLUTE_MAX_BLADE_DIAMETER, 
-            min(lawn_width / MAX_LAWN_DIVISION_FACTOR, lawn_length / MAX_LAWN_DIVISION_FACTOR));
-
-        const unsigned int MOVER_SIZE_MULTIPLICATON_FACTOR = 2;
         MIN_MOVER_WIDTH = MIN_BLADE_DIAMETER;
-        MAX_MOVER_WIDTH = MOVER_SIZE_MULTIPLICATON_FACTOR * MAX_BLADE_DIAMETER;
+        MAX_MOVER_WIDTH = Constants::MOVER_SIZE_MULTIPLICATON_FACTOR * MAX_BLADE_DIAMETER;
         MIN_MOVER_LENGTH = MIN_MOVER_WIDTH;
         MAX_MOVER_LENGTH = MAX_MOVER_WIDTH;
         
@@ -46,15 +40,10 @@ namespace Config {
 
         HORIZONTAL_FIELDS_NUMBER = static_cast<unsigned int>(round(static_cast<double>(lawn_width) / FIELD_WIDTH));
         VERTICAL_FIELDS_NUMBER = static_cast<unsigned int>(round(static_cast<double>(lawn_length) / FIELD_WIDTH));
-
-        const unsigned int ABSOLUTE_MIN_SPEED = 10;
-        const unsigned int ABSOLUTE_MAX_SPEED = 1000;
-        const unsigned int MIN_SPEED_DIVISION_FACTOR = 1000;
-        const unsigned int MAX_SPEED_DIVISION_FACTOR = 10;
         
-        MIN_SPEED = max(ABSOLUTE_MIN_SPEED, min(lawn_width / MIN_SPEED_DIVISION_FACTOR, 
-            lawn_length / MIN_SPEED_DIVISION_FACTOR));
-        MAX_SPEED = min(ABSOLUTE_MAX_SPEED, min(lawn_width / MAX_SPEED_DIVISION_FACTOR, 
-            lawn_length / MAX_SPEED_DIVISION_FACTOR));
+        MIN_SPEED = max(Constants::ABSOLUTE_MIN_SPEED, min(lawn_width / Constants::MIN_SPEED_DIVISION_FACTOR, 
+            lawn_length / Constants::MIN_SPEED_DIVISION_FACTOR));
+        MAX_SPEED = min(Constants::ABSOLUTE_MAX_SPEED, min(lawn_width / Constants::MAX_SPEED_DIVISION_FACTOR, 
+            lawn_length / Constants::MAX_SPEED_DIVISION_FACTOR));
     }
 }
