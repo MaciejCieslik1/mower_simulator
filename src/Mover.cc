580317bc2d7eb6ccc5db13bc016343e3dc7cfa70
Maciej <01187214@pw.edu.pl>
2025-11-24 20:15:40 +0100

    Implemneted cosntructor and getters

---
+++ src/Mover.cc
@@ -0,0 +1,50 @@
+/* 
+    Author: Maciej Cieslik
+    
+    Implements mover class.
+*/
+
+#include "../include/Config.h"
+#include "../include/Mover.h"
+
+using namespace std;
+
+
+Mover::Mover(const unsigned int& width, const unsigned int& length, const unsigned int& blade_diameter,
+        const unsigned int& speed) : width_(width), length_(length), blade_diameter_(blade_diameter), speed_(speed), 
+        angle_(Config::STARTING_ANGLE), x_(Config::STARTING_X), y_(Config::STARTING_Y) {}
+
+
+unsigned int Mover::getWidth() const {
+    return width_;
+}    
+
+
+unsigned int Mover::getLength() const {
+    return length_;
+}
+
+
+unsigned int Mover::getBladeDiameter() const {
+    return blade_diameter_;
+}
+
+
+unsigned int Mover::getSpeed() const {
+    return speed_;
+}
+
+
+unsigned short Mover::getAngle() const {
+    return angle_;
+}
+
+
+double Mover::getX() const {
+    return x_;
+}
+
+
+double Mover::getY() const {
+    return y_;
+}
