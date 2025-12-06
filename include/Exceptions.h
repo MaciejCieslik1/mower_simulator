0aa74e950dccfc7c1532e8f31d4d98f2d301342e
Maciej <01187214@pw.edu.pl>
2025-12-06 23:05:39 +0100

    Added RotationAngleOutOfRangeError declaration

--- include/Exceptions.h
+++ include/Exceptions.h
@@ -12,3 +12,13 @@ public:
 
     const char* what() const noexcept override;
 };
+
+
+class RotationAngleOutOfRangeError : public std::exception {
+private:
+    std::string msg;
+public:
+    explicit RotationAngleOutOfRangeError(const std::string& message);
+
+    const char* what() const noexcept override;
+};
